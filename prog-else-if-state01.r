ebcdb12dc693a2d9df11278a3cb5ceed99c186b4
Humesh Deshmukh <112746090+humeshdeshmukh@users.noreply.github.com>
2023-08-07 22:40:39 +0530

    new commit

--- prog-else-if-state01.r
+++ prog-else-if-state01.r
@@ -1,12 +1,13 @@
-age <- readline(prompt="Enter age: ")  
-age <- as.integer(age)  
-if(age<18){
-  print("You are child")  
-}else{
-  {if(age>30)  
-  print("You are old guy")  
-}else{
-  print("You are adult")  
-}
+age <- readline(prompt = "Enter age: ")
+age <- as.integer(age)
+if (age < 18) {
+  print("You are child")
+} else {
+  {
+    if (age > 30)
+      print("You are old guy")
+  } else {
+    print("You are adult")
+  }
 }
 
